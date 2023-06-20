9720e41d1e314e77a40f3b1f3361808cc1f592bc
MuskanJaiswal11037 <102662949+MuskanJaiswal11037@users.noreply.github.com>
2023-06-20 15:38:05 +0530

    Add files via upload

---
+++ backend/controllers/register.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const pool = require('./dbConfig');
+const bcrypt = require("bcrypt");
+const hostname = '127.0.0.1';
+const port = process.env.PORT||5000;
+const cors=require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/register", async(req,res) => {
+    
+    const { firstName,lastName, email, password,mobileNumber,DOB,street_address,city,region,city_pincode } = req.body;  
+    let Query2, Query1 ;
+    let hashedPassword = await bcrypt.hash(password,10);
+   
+    Query1=  await pool.query("select email from user where email=$1 ",[email]);
+
+    if(!Query1.rows.length)
+    {   Query2 = await pool.query(
+        "INSERT INTO user values(default,$1,$2,$3,$4,$5,$6,$7,$8,$9,$10)",[firstName,lastName, email, hashedPassword,mobileNumber,DOB,street_address,city,region,city_pincode]
+    );
+    
+    }
+})
+
+// app.listen(port,hostname, () => {
+//     console.log(`Here we go, Engines started at ${port}.`);
+
+// })
+
+
