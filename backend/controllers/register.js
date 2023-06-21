ff1f2110dba95a40598873f74bf78881046f6a8b
MuskanJaiswal11037 <102662949+MuskanJaiswal11037@users.noreply.github.com>
2023-06-21 18:48:13 +0530

    Update register.js

--- backend/controllers/register.js
+++ backend/controllers/register.js
@@ -3,16 +3,14 @@ const pool = require('./dbConfig');
 const bcrypt = require("bcrypt");
 
 exports.Register =async function register(req,res) {
-    const { firstName,lastName, email, password,mobileNumber,DOB,street_address,city,region,city_pincode } = req.body;  
-    let Query2, Query1 ;
-    let hashedPassword = await bcrypt.hash(password,10);
-    Query1=  await pool.query("select email from user where email=$1 ",[email]);
-
+ const { CustomerDetails } = req.body;  
+    console.log(CustomerDetails.lastName);
+    let Query2, Query1;
+    let hashedPassword = await bcrypt.hash(CustomerDetails.password,10);
+    Query1=  await pool.query("select email from user where email=$1 ",[CustomerDetails.email]);
     if(!Query1.rows.length)
     {   Query2 = await pool.query(
-        "INSERT INTO user values(default,$1,$2,$3,$4,$5,$6,$7,$8,$9,$10)",[firstName,lastName, email, hashedPassword,mobileNumber,DOB,street_address,city,region,city_pincode]
-    );
-    
-    }
+        "INSERT INTO user values(default,$1,$2,$3,$4,$5,$6,$7,$8,$9,$10)",[CustomerDetails.firstName,CustomerDetails.lastName, CustomerDetails.email, hashedPassword,CustomerDetails.mobileNumber,CustomerDetails.DOB,CustomerDetails.street_address,CustomerDetails.city,CustomerDetails.region,CustomerDetails.city_pincode]
+    );}
 }
 
