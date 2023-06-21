42b2e83a3c7a01fb450e6f86ecf94b3da24e64a6
MuskanJaiswal11037 <102662949+MuskanJaiswal11037@users.noreply.github.com>
2023-06-21 15:10:25 +0530

    Update register.js

--- backend/controllers/register.js
+++ backend/controllers/register.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const pool = require('./dbConfig');
 const bcrypt = require("bcrypt");
-const hostname = '127.0.0.1';
-const port = process.env.PORT||5000;
 const cors=require("cors");
 const app = express();
 app.use(cors());
