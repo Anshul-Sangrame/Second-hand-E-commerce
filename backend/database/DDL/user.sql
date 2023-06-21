4c0352ea9c788a76c98add7ff6dd44040bc71c6e
MuskanJaiswal11037 <102662949+MuskanJaiswal11037@users.noreply.github.com>
2023-06-21 19:04:52 +0530

    Update user.sql

--- backend/database/DDL/user.sql
+++ backend/database/DDL/user.sql
@@ -1,13 +1,7 @@
-CREATE TABLE IF NOT EXISTS user(
-    id BIGSERIAL  ,
-    firstname varchar(100) NOT NULL,
-    lastname varchar(100) NOT NULL,
-    email varchar(100) NOT NULL UNIQUE PRIMARY KEY,
-    password varchar(50) NOT NULL,
-    phonenumber int NOT NULL,
-    DateofBirth  date NOT NULL,
-    street_address varchar(100) NOT NULL,
-    city varchar(50) NOT NULL,
-    region varchar(50) NOT NULL,
-    city_pincode varchar(100) NOT NULL,
+CREATE TABLE IF NOT EXISTS users(
+   id SERIAL PRIMARY KEY,
+   name VARCHAR(30) NOT NULL UNIQUE,
+   email VARCHAR(200) NOT NULL,
+   username VARCHAR(30) NOT NULL,
+   password TEXT NOT NULL
 );
