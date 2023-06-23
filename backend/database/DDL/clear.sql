4f17ff259019c0ec38516151dbff5014472b90bd
Anshul-Sangrame <anshul.sangrame@gmail.com>
2023-06-23 20:50:52 +0530

    DDL files

---
+++ backend/database/DDL/clear.sql
@@ -0,0 +1,4 @@
+DROP TABLE ratings;
+DROP TABLE carts;
+DROP TABLE products;
+DROP TABLE users;
---
+++ backend/database/DDL/reset.sql
@@ -0,0 +1,3 @@
+\i '/home/anshul/programming/webDev/Second-hand-E-commerce/backend/database/DDL/clear.sql'
+\i '/home/anshul/programming/webDev/Second-hand-E-commerce/backend/database/DDL/entities.sql'
+\i '/home/anshul/programming/webDev/Second-hand-E-commerce/backend/database/DDL/relational_set.sql'
