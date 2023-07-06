ca5eba507814facf8d3d96ce362d4baaccff7bc6
harshkhandelwal97 <136146238+harshkhandelwal97@users.noreply.github.com>
2023-07-06 11:30:29 +0530

    Update Login.jsx

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -76,35 +76,5 @@ function Login() {
       </div>
     </div>
   );
-};function Login(){
-  return (
-    <div className="main">
-      <div className="sub-main">
-        <div>
-          <div className="imgs">
-            <div className="container-image">
-              <img src={profile} alt ="profile" className="profile"/>
-
-            </div>
-          </div>
-          <div>
-            <h1>Login to your account</h1>
-            <div>
-              <img src={user} alt="user" className="user"/>
-              <input type="text" placeholder="user-name" className="name"/>
-            </div>
-            <div>
-              <img src={password} alt="password" className="password"/>
-              <input type="text" placeholder="Password" className="name"/>
-            </div>
-            <button className="login-button">Login</button>
-          </div>
-          <p className="link">
-              <a href="#" className="l">Forgot password ?</a> Or<a href="#" className="l">Sign Up</a>
-            </p>
-        </div>
-      </div>
-    </div>
-  );
 };
 export default Login;
