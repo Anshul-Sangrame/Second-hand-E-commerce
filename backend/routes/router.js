345838abdbdb2c6f901243221945569c12078325
harshkhandelwal97 <136146238+harshkhandelwal97@users.noreply.github.com>
2023-07-12 14:12:06 +0530

    Update router.js

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -2,37 +2,36 @@ import { Router } from "express";
 import VerifyToken from "../controllers/verifyToken.js";
 import login from "../controllers/login.js";
 import register from "../controllers/register.js";
-import {EditProfileGet, EditProfilePost }from "../controllers/EditProfile.js";
+import { EditProfileGet, EditProfilePost } from "../controllers/EditProfile.js";
 import pool from "../database/db.js";
 import bcrypt from 'bcrypt'
 import productDetails from "../controllers/productDetails.js";
-import Sell from "../controllers/sell.js";
+import myProducts from "../controllers/myProduct.js";
+import sell from "../controllers/sell.jsx";
 
-const router = Router()
+const router = Router();
+export const publicRouter = Router();
 
 // Verifies JWT tokens
 router.use(VerifyToken);
 
-// routes
-router.post('/sell',Sell);
-router.post('/login', login)
-router.post('/register', register)
-router.get('/editprofile',EditProfileGet)
-router.post('/editprofile',EditProfilePost)
+// public routes
+publicRouter.post('/login', login)
+publicRouter.post('/register', register)
+// Private
+router.post('/sell', SellPost);
+router.get('/editprofile', EditProfileGet)
+router.post('/editprofile', EditProfilePost)
 router.post('/register', async (req, res) => { register.Register(req, res) });
 router.get('/verify', (req, res) => {
     try {
-
-        if (req.user_id) {
-            return res.json({ id: req.user_id });
-        }
-        console.log("unauthorized")
-        return res.status(401).send("unauthorized")
+        res.json({ id: req.user_id });
     } catch (err) {
         console.error(err.message);
+        res.status(500).send("server error");
     }
 })
-
+router.get('/myProducts', myProducts);
 router.get('/productDetails/:id', productDetails);
 
 // homepage
@@ -40,7 +39,7 @@ router.get('/home', async (req, res) => {
     try {
         if (req.user_id) {
             const result = await pool.query('SELECT id, tag, title, cost, views, image_url, qty FROM products ORDER BY views DESC LIMIT 10');
-            return res.json({data: result.rows});
+            return res.json({ data: result.rows });
         }
         return res.json(null);
 
@@ -61,15 +60,17 @@ router.get('/hashAll', async (req, res) => {
         const result = await pool.query(preparedStmt);
 
         for (let item of result.rows) {
-            const salt = await bcrypt.genSalt(10);
-            const Hashedpassword = await bcrypt.hash(item.password, salt)
-            preparedStmt = {
-                text: "UPDATE users \
-                        SET password = $1 \
-                        WHERE id=$2",
-                values: [Hashedpassword, item.id]
+            if (item.password === "hashed_password") {
+                const salt = await bcrypt.genSalt(10);
+                const Hashedpassword = await bcrypt.hash(item.password, salt)
+                preparedStmt = {
+                    text: "UPDATE users \
+                            SET password = $1 \
+                            WHERE id=$2",
+                    values: [Hashedpassword, item.id]
+                }
+                await pool.query(preparedStmt);
             }
-            await pool.query(preparedStmt);
         }
         res.json(result.rows);
     } catch (err) {
